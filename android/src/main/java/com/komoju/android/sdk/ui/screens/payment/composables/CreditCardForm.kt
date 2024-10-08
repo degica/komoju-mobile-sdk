4187bdc8ce2cb182e2e36da3db3e613ebf45637f
Aman tonk <amantonkem@gmail.com>
2024-10-08 15:25:55 +0900

    Added UI test for maestro (#28)
    * Added UI test for maestro
    
    * ktlint fixups

--- android/src/main/java/com/komoju/android/sdk/ui/screens/payment/composables/CreditCardForm.kt
+++ android/src/main/java/com/komoju/android/sdk/ui/screens/payment/composables/CreditCardForm.kt
@@ -25,6 +25,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onGloballyPositioned
@@ -54,8 +55,10 @@ import com.komoju.android.sdk.utils.CreditCardUtils.formatDinnersClub
 import com.komoju.android.sdk.utils.CreditCardUtils.formatOtherCardNumbers
 import com.komoju.android.sdk.utils.CreditCardUtils.identifyCardScheme
 import com.komoju.android.sdk.utils.CreditCardUtils.makeExpirationFilter
+import com.komoju.android.sdk.utils.testID
 import com.komoju.mobile.sdk.entities.PaymentMethod
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 internal fun CreditCardForm(
     creditCard: PaymentMethod.CreditCard,
@@ -206,7 +209,7 @@ internal fun CreditCardForm(
 
         Spacer(modifier = Modifier.height(8.dp))
         PrimaryButton(
-            modifier = Modifier
+            modifier = Modifier.testID("credit_card_pay")
                 .padding(horizontal = 16.dp)
                 .fillMaxWidth(),
             text = "${LocalI18nTexts.current["PAY"]} $displayPayableAmount",
