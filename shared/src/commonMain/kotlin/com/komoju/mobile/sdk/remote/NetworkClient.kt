3fe96e2c9a70fe7c27e5f0ab1a7b63b0ec383500
amnix <amantonkem@gmail.com>
2024-10-09 15:36:54 +0900

    Added identification Headers + Locked API Version

--- shared/src/commonMain/kotlin/com/komoju/mobile/sdk/remote/NetworkClient.kt
+++ shared/src/commonMain/kotlin/com/komoju/mobile/sdk/remote/NetworkClient.kt
@@ -4,7 +4,6 @@ import io.ktor.client.HttpClient
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.serialization.kotlinx.json.json
-import io.ktor.util.appendIfNameAbsent
 import io.ktor.util.encodeBase64
 import kotlinx.serialization.json.Json
 
@@ -13,9 +12,11 @@ private const val BASE_URL = "https://komoju.com/api/"
 internal fun createNetworkClient(publishableKey: String?) = HttpClient {
     defaultRequest {
         url(BASE_URL)
-        headers.append("authorization", "Basic ${publishableKey?.encodeBase64()}")
-        headers.appendIfNameAbsent("Accept", "application/json")
-        headers.appendIfNameAbsent("Content-Type", "application/json")
+        headers.append("Authorization", value = "Basic ${publishableKey?.encodeBase64()}")
+        headers.append("KOMOJU-VIA", "mobile_${PLATFORM.lowercase()}")
+        headers.append("X-KOMOJU-API-VERSION", "2024-07-15")
+        headers.append("Accept", "application/json")
+        headers.append("Content-Type", "application/json")
     }
     install(ContentNegotiation) {
         json(
@@ -27,3 +28,5 @@ internal fun createNetworkClient(publishableKey: String?) = HttpClient {
         )
     }
 }
+
+expect val PLATFORM: String
